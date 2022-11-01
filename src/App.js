95f00eae0d598896cab35c86acba9c4c6de85e65
waladi98 <waladywidarno@gmail.com>
2022-11-01 13:53:57 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container py-4 ">
+        <h1>React Table</h1>
+        <h4>Create Reuse table component</h4>
+        <button className="btn btn-danger">
+          <em className="fas fa-trash"></em>
+        </button>
+      </div>
     </div>
   );
 }
